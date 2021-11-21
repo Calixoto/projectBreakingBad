2bacf10a4d5aa8590db69057c9d94556e3b45000
calixoto <grimmjow006@live.com>
2021-11-21 16:19:47 -0300

    modal

--- src/components/Items/index.tsx
+++ src/components/Items/index.tsx
@@ -1,19 +1,53 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import style from './styles.module.scss';
+import { GrClose } from 'react-icons/gr';
 
 interface CardProps {
-    // isLoading: boolean;
-    // items: any;
     item: any;
 }
 
 export default function Items({ item }: CardProps) {
-    console.log(item);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    function handleOpenModal() {
+        setIsModalOpen(true);
+    }
+
+    function handleCloseModal() {
+        setIsModalOpen(false);
+    }
     return (
         <div className={style.cardContent}>
             <img src={item.img} alt="" />
             <h3>{item.name}</h3>
             <h4>{item.nickname}</h4>
-            <a href="#">Ler mais</a>
+            <button onClick={handleOpenModal}>Ler mais</button>
+            <Modal
+                isOpen={isModalOpen}
+                onRequestClose={handleCloseModal}
+                overlayClassName={style.charModalOverlay}
+                className={style.charModalContent}
+            >
+                <button
+                    type="button"
+                    className={style.closeModal}
+                    onClick={handleCloseModal}
+                >
+                    <GrClose />
+                </button>
+                <h1>{item.name}</h1>
+                <div>
+                    <strong>Ator:  <span>{item.portrayed}</span></strong>
+                    <strong>Aniversário:  <span>{item.birthday}</span></strong>
+                    <strong>Ocupação:  <span>{item.occupation}</span></strong>
+                    <strong>Status:  <span>{item.status}</span></strong>
+                    <strong>Episodios:  <span>1 2 3 4</span></strong>
+                </div>
+
+                <h1></h1>
+
+            </Modal>
         </div>
     );
 }
